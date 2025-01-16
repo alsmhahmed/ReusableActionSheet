2341417fff0cef4b217bd6707ae3ef3d55d601d1
alsmhahmed <alsmh.ahmed@gen-c.com>
2025-01-16 19:28:34 +0200

    Initial Commit

---
+++ ReusableActionSheet/ReusableActionSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReusableActionSheetApp.swift
+//  ReusableActionSheet
+//
+//  Created by alsmh ahmed on 16/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ReusableActionSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
